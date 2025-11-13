89d26f8afdc9aee9f98069b785a720452af6d242
DZL <i@deifzar.me>
2025-11-13 11:59:27 +0100

    Initiliase RabbitMQ queues and consumers only when the API service is ready

--- pkg/api8/api8.go
+++ pkg/api8/api8.go
@@ -89,8 +89,7 @@ func (a *Api8) Init() error {
 // This prevents consumers from receiving messages before the API can handle them.
 func (a *Api8) InitializeConsumerAfterReady() {
 	go func() {
-		locationService := a.Config.GetString("ORCHESTRATORM8.Services.reportingm8")
-		requestURL := locationService + "/health"
+		requestURL := "http://localhost:8004/health"
 
 		log8.BaseLogger.Info().Msg("Waiting for API service to become ready before activating RabbitMQ consumer...")
 
